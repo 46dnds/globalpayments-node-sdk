bfc2d02c231ad966da5ada4278b3262520a1c550
Francis Levasseur <f.levasseur@azimutpos.com>
2023-12-28 10:39:18 -0500

    convert https wrapper to node-fetch so we can easily implement corporate proxy

--- src/Gateways/https-wrapper.ts
+++ src/Gateways/https-wrapper.ts
@@ -1,41 +1,46 @@
-import * as https from "https";
-import { Socket } from "net";
-
+const HttpsProxyAgent = require('https-proxy-agent');
+const fetch = require('node-fetch');
 import { ApiError, GatewayError } from "../Entities/Errors";
-
+const _fetch=(url: string,options: Object)=>{
+  if (options === undefined) {
+    options = {};
+  }
+  return new Promise((resolve, reject) => {
+    const c = new AbortController();
+    const timer = setTimeout(() => {
+      c.abort(new ApiError("Socket timeout occurred."));
+    }, options.timeout || 1000*60*5)
+    fetch(url,{...options,signal:c.signal,agent: process.env.https_proxy?new HttpsProxyAgent(process.env.https_proxy):null})
+    .then(value => {
+      clearTimeout(timer)
+      resolve(value)
+    })
+    .catch(reason => {
+      clearTimeout(timer)
+      reject(reason)
+    })
+  });
+};
 export const request = (
   data?: string,
-  options?: https.RequestOptions,
+  options?: Object,
 ): Promise<string> =>
   new Promise((resolve, reject) => {
     if (options === undefined) {
       options = {};
     }
-
-    const req = https.request(options, (res) => {
-      let responseData = "";
-
-      res.on("data", (d: string) => (responseData += d));
-      res.on("end", () => {
-        if (res.statusCode !== 200) {
-          reject(
-            new GatewayError(`Unexpected HTTP status code [${res.statusCode}]`),
-          );
-        }
-        resolve(responseData);
-      });
-      res.on("error", reject);
-    });
-    req.on("socket", (socket: Socket) => {
-      socket.setTimeout(this.timeout || 100000);
-      socket.on("timeout", () => {
-        req.abort();
-        reject(new ApiError("Socket timeout occurred."));
-      });
-    });
-    req.on("error", reject);
-    if (data !== undefined) {
-      req.write(data);
-    }
-    req.end();
+    _fetch('https://'+options.host+(options.port && options.port!=443?':'+options.port:'')+options.path,{
+      method:options.method.toUpperCase(),
+      headers:options.headers,
+      timeout:this.timeout || 100000,
+      body:data
+    }).then((ans)=>{
+      if(ans.status==200){
+        ans.text().then((body)=>{
+            resolve(body);
+        });
+        return;
+      }
+      reject(new GatewayError(`Unexpected HTTP status code [${ans.status}]`));
+    }).catch(reject);
   });
